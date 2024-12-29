55c86021ebb903e38cf848610132765c3faa31b7
mahbubsakib <sakibmahbub92@gmail.com>
2024-12-29 11:39:50 +0600

    navbar, footer

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 const MainLayout = () => {
     return (
-        <div>
+        <div className='max-w-screen-xl mx-auto'>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
