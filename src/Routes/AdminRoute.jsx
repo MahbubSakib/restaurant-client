47b22e41a6c659523987f61a068b85dc6d5cea73
mahbubsakib <sakibmahbub92@gmail.com>
2025-01-12 20:51:14 +0600

    admin route

--- src/Routes/AdminRoute.jsx
+++ src/Routes/AdminRoute.jsx
@@ -1,10 +1,25 @@
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+import useAdmin from "../hooks/useAdmin";
+import { Navigate, useLocation } from "react-router-dom";
+import Loader from "../components/Loader";
 
-const AdminRoute = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const AdminRoute = (children) => {
+    const [user, loading] = useContext(AuthContext);
+    const [isAdmin, isAdminLoading]= useAdmin();
+    const location = useLocation();
+
+    if (loading || isAdminLoading) {
+        return <Loader></Loader>
+    }
+
+    if(user && isAdmin){
+        return children;
+    }
+
+    if (!user) {
+        return <Navigate state={location.pathname} to="/login" replace />;
+    }
 };
 
 export default AdminRoute;
